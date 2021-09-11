f786f4b6b6d4d9daf8aae18afdbba2d11905225b
addetz <adelinasimion@gmail.com>
2021-09-11 13:15:33 +0100

    hello world example

---
+++ basics/hello_world.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	hello()
+	goodbye()
+}
+
+func hello() {
+	fmt.Println("Hello, world!")
+}
+
+func goodbye() {
+	fmt.Println("Goodbye, world!")
+}
