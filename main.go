47f12e2b3d14da42cbe082b9be873e7816c2dc8c
addetz <adelinasimion@gmail.com>
2021-10-03 23:08:50 +0100

    add goodbyes slice

--- main.go
+++ main.go
@@ -6,26 +6,33 @@ import (
 )
 
 // greetings in many languages
-var greetings = []string{"Hello!", "Ciao!", "Hola!", "Hej!", "Salut!"}
+var hellos = []string{"Hello!", "Ciao!", "Hola!", "Hej!", "Salut!"}
+var goodbyes = []string{"Goodbye!", "Arrivederci!", "Adios!", "Hej Hej!", "La revedere!"}
 
 func main() {
 	// create a channel
 	ch := make(chan string, 1)
+	ch2 := make(chan string, 1)
 	// start the greeter to provide a greeting
-	go greet(ch)
+	go greet(hellos, ch)
+	go greet(goodbyes, ch2)
 	// sleep for a long time
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
 	fmt.Println("Main ready!")
-	for greeting := range ch{
-		// sleep and print
-		time.Sleep(2 * time.Second)
-		fmt.Println("Greeting received!", greeting)
+	for {
+		select {
+		case gr := <-ch:
+			printGreeting(gr)
+		case gr2 := <-ch2:
+			printGreeting(gr2)
+		default:
+			return
+		}
 	}
-
 }
 
 // greet writes a greet to the given channel and then says goodbye
-func greet(ch chan<- string) {
+func greet(greetings []string, ch chan<- string) {
 	fmt.Println("Greeter ready!")
 	// greet
 	for _, g := range greetings {
@@ -33,4 +40,11 @@ func greet(ch chan<- string) {
 	}
 	close(ch)
 	fmt.Println("Greeter completed!")
+}
+
+// printGreeting sleeps and prints the greeting given
+func printGreeting(greeting string) {
+	// sleep and print
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("Greeting received!", greeting)
 }
