c78a5cd6dfd984d6dd0d97559bbcf222c53a2529
addetz <adelinasimion@gmail.com>
2021-11-17 16:52:45 +0000

    implement the sync.Map

--- db/products.go
+++ db/products.go
@@ -3,22 +3,21 @@ package db
 import (
 	"fmt"
 	"sort"
+	"sync"
 
 	"github.com/applied-concurrency-in-go/models"
 	"github.com/applied-concurrency-in-go/utils"
 )
 
 type ProductDB struct {
-	products map[string]models.Product
+	products sync.Map
 }
 
 // NewProducts creates a new empty product DB
 func NewProducts() (*ProductDB, error) {
-	p := &ProductDB{
-		products: make(map[string]models.Product),
-	}
+	p := &ProductDB{}
 	// load start position
-	if err := utils.ImportProducts(p.products); err != nil {
+	if err := utils.ImportProducts(&p.products); err != nil {
 		return nil, err
 	}
 
@@ -27,7 +26,7 @@ func NewProducts() (*ProductDB, error) {
 
 // Exists checks whether a product with a give id exists
 func (p *ProductDB) Exists(id string) error {
-	if _, ok := p.products[id]; !ok {
+	if _, ok := p.products.Load(id); !ok {
 		return fmt.Errorf("no product found for id %s", id)
 	}
 
@@ -36,27 +35,39 @@ func (p *ProductDB) Exists(id string) error {
 
 // Find returns a given product if one exists
 func (p *ProductDB) Find(id string) (models.Product, error) {
-	prod, ok := p.products[id]
+	pp, ok := p.products.Load(id)
 	if !ok {
 		return models.Product{}, fmt.Errorf("no product found for id %s", id)
 	}
 
-	return prod, nil
+	return toProduct(pp), nil
 }
 
 // Upsert creates or updates a product in the orders DB
 func (p *ProductDB) Upsert(prod models.Product) {
-	p.products[prod.ID] = prod
+	p.products.Store(prod.ID, prod)
 }
 
 // FindAll returns all products in the system
 func (p *ProductDB) FindAll() []models.Product {
 	var allProducts []models.Product
-	for _, product := range p.products {
-		allProducts = append(allProducts, product)
-	}
+	p.products.Range(func(_, value interface{}) bool {
+		allProducts = append(allProducts, toProduct(value))
+		return true
+	})
+
 	sort.Slice(allProducts, func(i, j int) bool {
 		return allProducts[i].ID < allProducts[j].ID
 	})
 	return allProducts
+}
+
+// toProduct attempts to convert an interface to a product
+// panics if if this not possible
+func toProduct(pp interface{}) models.Product {
+	prod, ok := pp.(models.Product)
+	if !ok {
+		panic(fmt.Errorf("error casting %v to product", pp))
+	}
+	return prod
 }
